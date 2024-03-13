edea4803e009357a0aa0ca8e1f1dc77f0ad5d77d
JFeo390w9tgf <163280249+JFeo390w9tgf@users.noreply.github.com>
2024-03-13 15:51:21 +0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,15 @@
+import pyautogui as spam
+import time
+
+limit = input("Jumlah pesan : ")
+msg = input("isi pesan : ")
+
+i = 0
+
+time.sleep(5)
+
+while i<int(limit):
+	spam.typewrite(msg)
+	spam.press('Enter')
+
+i+=1
